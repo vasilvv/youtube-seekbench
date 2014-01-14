9491f405b14c00d3fda247fa1f590d90fba0ca61
Victor Vasiliev <vasilvv@mit.edu>
2014-01-15 02:01:15 +0400

    Add script to launch browser.

---
+++ launch-browser.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Classic bash snippet for script directory (from StackOverflow)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+chromium-browser --disable-plugins --disable-background-networking --user-data-dir=/tmp/alfalfa "$DIR/monkey.html"
