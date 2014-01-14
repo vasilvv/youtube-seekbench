24e549b00c12ff21bcc3bc2ca2059b3adb623574
Victor Vasiliev <vasilvv@mit.edu>
2014-01-15 02:34:07 +0400

    Add the flag which allows us to introspect the state of the player.

--- launch-browser.sh
+++ launch-browser.sh
@@ -3,4 +3,4 @@
 # Classic bash snippet for script directory (from StackOverflow)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-chromium-browser --disable-plugins --disable-background-networking --user-data-dir=/tmp/alfalfa "$DIR/monkey.html"
+chromium-browser --disable-web-security --disable-plugins --disable-background-networking --user-data-dir=/tmp/alfalfa "$DIR/monkey.html"
